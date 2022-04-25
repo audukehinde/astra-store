e0ccff433fd76c9a95c35bf8760d28c2647167d6
audukehinde <auduomohkehinde@gmail.com>
2022-04-25 08:02:43 -0400

    Add navigation bar, add button, and table

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      myblue: '#162A46',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
     extend: {},
   },
   plugins: [],
