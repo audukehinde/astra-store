00dc8601b94927d246d50c1fb96421490c571920
Aminat Adeshewa <adeshewaayoni@gmail.com>
2022-04-28 13:17:14 +0100

    Add description page resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkblackblue: 'rgb(22, 42, 70, 100%)',
+        lightwhiteblue: 'rgb(220, 234, 253, 100%)'
+      },
+    },
   },
   plugins: [],
 }
