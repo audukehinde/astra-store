35716f440820a515c041baafa39cda520e3966d9
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-04-28 07:46:03 +0100

    cart page and order summary added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
       colors: {
         mainBlue: 'rgb(22, 42, 70)',
         secondaryPink: 'rgba(254, 58, 58, 0.99)',
+        mainGray: 'rgb(196,196,196,0.28)',
+        buttonPink: 'rgba(255, 112, 112, 0.99)',
       },
     },
   },
