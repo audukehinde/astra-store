15b8ea2ee0f53c8c2ef375554e0f168d4b9ab375
pelummie <apelummie@yahoo.com>
2022-05-02 20:07:52 +0800

    Add login page resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      myblue: '#162A46',
+      red: '#FF0000',
+      'white': '#ffffff',
+      'grey': '#c4c4c4',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
     extend: {},
   },
   plugins: [],
